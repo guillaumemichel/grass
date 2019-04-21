09251d5535514729633036070a151a237fb3e972
Xavier Pantet <25949462+xavierpantet@users.noreply.github.com>
2019-04-21 22:33:08 +0200

    Added documentation

--- include/AuthorizationService.h
+++ include/AuthorizationService.h
@@ -1,13 +1,24 @@
+/**
+ * AuthorizationService.h
+ * Determined which resources a given user is allowed to access.
+ * See AuthorizationPolicies.h for details.
+ * Author: Xavier Pantet
+ */
+
 #include <string>
 #include <map>
-
 #include "AuthorizationPolicy.h"
+
 #ifndef USER
 #include "User.h"
 #endif
 
 using namespace std;
 
+/**
+ * Creates a map that associates a policy level to every resource.
+ * @return Policy level - Resource association for every resource of the system.
+ */
 map<string, AuthorizationPolicy> init_policies();
 
 class AuthorizationService {
@@ -15,12 +26,17 @@ class AuthorizationService {
         const User& user;
 
     public:
-        // Constructor and destructor
-        AuthorizationService(const User&);
 
-        // Static attributes
+        explicit AuthorizationService(const User&);
+
+        /**
+         * Policy level required to access a given resource for any resource.
+         */
         static map<string, AuthorizationPolicy> policies;
 
-        // Class methods
-        bool hasAccessTo(const string);
+        /**
+         * Determines whether the user has access to the given resource.
+         * @return true iff the user has access to the given resource
+         */
+        bool hasAccessTo(const string resource);
 };
--- include/User.h
+++ include/User.h
@@ -1,3 +1,8 @@
+/**
+ * User.h
+ * Defined the notion of user within the system.
+ * Author: Xavier Pantet
+ */
 #ifndef USER
 #define USER
 
@@ -7,8 +12,23 @@ using namespace std;
 class User {
 public:
     User(const string name);
-    void setAuthenticated(bool);
+
+    /**
+     * Sets the authentication status of the user.
+     * @param status The new authentication status
+     */
+    void setAuthenticated(bool status);
+
+    /**
+     * Gets the authentication status of the user.
+     * @return true iff the user has been authenticated
+     */
     bool isAuthenticated() const;
+
+    /**
+     * Gets name of the user
+     * @return User name
+     */
     string getName() const;
 
 private:
